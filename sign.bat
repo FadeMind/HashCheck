53aa6f5d12fa320e7a12244a8766a4b94325d537
Mounir IDRASSI <mounir.idrassi@amcrypto.jp>
2025-05-20 23:03:34 +0900

    Update code signing certificate

--- sign.bat
+++ sign.bat
@@ -1,10 +1,10 @@
-PATH=%PATH%;%WSDK81%\bin\x86;C:\dev\Progs\NSIS
+PATH=%PATH%;%WSDK81%\bin\x86;C:\dev\Progs\NSIS;C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x86;
 
 rem sign using SHA-256
-signtool sign /v /sha1 04141E4EA6D9343CEC994F6C099DC09BDD8937C9 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 "Bin\Win32\Release\HashCheck.dll" "Bin\x64\Release\HashCheck.dll" 
+signtool sign /v /sha1 86E1D426731E79117452F090188A828426B29B5F /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.digicert.com /td SHA256 "Bin\Win32\Release\HashCheck.dll" "Bin\x64\Release\HashCheck.dll" 
 
 makensis.exe installer\HashCheck.nsi
 
-signtool sign /v /sha1 04141E4EA6D9343CEC994F6C099DC09BDD8937C9 /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256  "installer\HashCheckSetup-v2.5.0.1.exe"
+signtool sign /v /sha1 86E1D426731E79117452F090188A828426B29B5F /ac GlobalSign_SHA256_EV_CodeSigning_CA.cer /fd sha256 /tr http://timestamp.digicert.com /td SHA256  "installer\HashCheckSetup-v2.5.0.1.exe"
 
 pause
